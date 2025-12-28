0f2742dafac64e1b2075cadbb1c3194d207845bf
MD MAHEDI HASAN <55445546+mookto@users.noreply.github.com>
2025-12-28 18:42:51 +0600

    Update Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -6,7 +6,8 @@ public class Palindrome {
         for(int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
         }
-
+//i-- প্রতিবার index কমাচ্ছে → 4 → 3 → 2 → 1 → 0 : reverse loop
+     //"+=" i er value add kora rev er moddhe       
         if(str.equals(rev))
             System.out.println(str + " is Palindrome");
         else
