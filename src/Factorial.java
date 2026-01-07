8aae97875ebd968476f7778210c40c9bbe954fb4
MD MAHEDI HASAN <55445546+mookto@users.noreply.github.com>
2026-01-07 15:45:46 +0600

    Update Factorial.java

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,12 +1,16 @@
 public class Factorial {
     public static void main(String[] args) {
-        int n = 5;
-        int fact = 1;
 
+        int n = 5;      // এখানে আমরা যে সংখ্যার factorial বের করব (n = 5)
+        int fact = 1;   // factorial এর মান স্টোর করার জন্য ভ্যারিয়েবল (শুরুতে 1 ধরা হয়)
+
+        // 1 থেকে n পর্যন্ত loop চলবে
         for(int i = 1; i <= n; i++) {
-            fact *= i;
+
+            fact *= i;  // fact = fact * i  (প্রতিবার fact এর সাথে বর্তমান i গুণ হবে)
         }
 
+        // শেষ পর্যন্ত fact এ factorial এর মান পাওয়া যাবে
         System.out.println("Factorial: " + fact);
     }
 }
