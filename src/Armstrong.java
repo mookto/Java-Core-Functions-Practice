d5d2667d00593c296796a7339cf4c23e6b7bd303
MD MAHEDI HASAN <55445546+mookto@users.noreply.github.com>
2026-01-07 17:56:08 +0600

    Update Armstrong.java

--- src/Armstrong.java
+++ src/Armstrong.java
@@ -1,13 +1,21 @@
 public class Armstrong {
     public static void main(String[] args) {
-        int num = 153, sum = 0, temp = num;
 
+        int num = 153;      // যেই সংখ্যাটা চেক করব
+        int sum = 0;        // digit cube এর যোগফল রাখবে
+        int temp = num;     // num এর কপি (temp দিয়ে হিসাব করব)
+
+        // temp যতক্ষণ 0 না হয় ততক্ষণ লুপ চলবে
         while(temp != 0) {
-            int digit = temp % 10;
-            sum += digit * digit * digit;
-            temp /= 10;
+
+            int digit = temp % 10;  // শেষ digit বের করি (ex: 153 -> 3)
+
+            sum += digit * digit * digit; // digit এর cube করে যোগ করি
+
+            temp /= 10;   // temp কে 10 দিয়ে ভাগ দিয়ে শেষ digit বাদ দেই (153 -> 15)
         }
 
+        // cube এর যোগফল num এর সমান হলে এটা Armstrong number
         if(sum == num)
             System.out.println(num + " is Armstrong");
         else
